525a6aaf4160d391db62f36c13c68c2c1e4a5d01
Badsha Hossain <badshab454@gmail.com>
2023-09-18 16:11:42 +0600

    base url

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,17 @@
+import {useState} from "react";
 import styled from "styled-components";
 
+const BASE_URL = "http://localhost:9000";
+
 const App = () => {
+  const [data, setData] = useState(null);
   return (
     <Container>
       <TopContainer>
         <div className="logo">
-          <a href="#"><img src="/logo.svg" alt="logo" /></a>
+          <a href="#">
+            <img src="/logo.svg" alt="logo" />
+          </a>
         </div>
         <div className="search">
           <input type="text" placeholder="Search Food" />
@@ -18,9 +24,7 @@ const App = () => {
         <Button>Dinner</Button>
       </FilterContainer>
       <FoodCardContainer>
-        <FoodCards>
-
-        </FoodCards>
+        <FoodCards></FoodCards>
       </FoodCardContainer>
     </Container>
   );
@@ -53,22 +57,25 @@ const TopContainer = styled.section`
 `;
 
 const FilterContainer = styled.section`
-display: flex;
-justify-content: center;
-gap: 10px;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  padding-bottom: 40px;
 `;
 
 const Button = styled.button`
-background: #ff4343;
-border-radius: 5px;
-padding: 6px 12px;
-border: none;
-cursor: pointer;
-color: #fff;
+  background: #ff4343;
+  border-radius: 5px;
+  padding: 6px 12px;
+  border: none;
+  cursor: pointer;
+  color: #fff;
 `;
 
 const FoodCardContainer = styled.section`
-background-image: url("bg.png");
+  background-size: cover;
+  height: calc(100vh - 210px);
+  background-image: url("bg.png");
 `;
 const FoodCards = styled.div``;
 
